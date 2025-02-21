52281dfc78ec8815e66b7b954eea4d4f98ea5e31
Arthur Rocha <65750989+DamiemB@users.noreply.github.com>
2025-02-21 14:36:49 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep
 from os import system
+from random import randint
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -9,20 +10,16 @@ from selenium.webdriver.support import expected_conditions as EC
 def main():
     
     credenciais = {
-        "cpf" : "",
-        "aniver" : ""
+        "user_cpf" : "",
+        "user_date" : "",
+        "user_token" : ""
     }
     driver = webdriver.Firefox()
-
-    def bruteforce():
-        token_brute_force = 9999
-        while token_brute_force != 0:
-            token_brute_force -= 1
-
     
     def user_input():
-        credenciais['cpf'] = input('Digite seu cpf; ')
-        credenciais['aniver'] = input('Digite seu aniversario no seguinte modelo (xx/xx/xxxx); ')
+        credenciais['user_cpf'] = input('Digite seu cpf; ')
+        credenciais['user_date'] = input('Digite seu aniversario no seguinte modelo (xx/xx/xxxx); ')
+        credenciais['user_token'] = input('Digite o Token do dia: ')
 
     def acessandoSite():
         driver.get("https://infinityschool.app/")
@@ -33,7 +30,6 @@ def main():
     def login(cpf, aniversario):
         login = driver.find_element(By.NAME, "projectFilePath")
         login.send_keys(cpf)
-
         senha = driver.find_element(By.NAME, "data")
         senha.send_keys( aniversario + Keys.ENTER)
         sleep(2)
@@ -44,20 +40,26 @@ def main():
         sleep(2)
 
     def marcandoPresenca():
-        botao2 = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Presença aula do curso')]")))
+        botao2 = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Presença aula do curso')]")))
         botao2.click()
         sleep(2)
     
     def validacaotoken():
         token = driver.find_element(By.NAME, "token_validacao")
-        token.send_keys("6666" + Keys.ENTER)
-        validation_message = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "validation-message")))
+        token.send_keys(credenciais['user_token'] + Keys.ENTER)
+        validation_message = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "validation-message")))
+        confirmar_presença_button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Concluir presença')]")))
         message_text = validation_message.text
         print(f'a mensagem de texto é >>> {message_text}')
         if message_text == "Token inválido!":
             print("Comparação de token invalida")
+            while menssage_text == "Token Inválido":
+                random = randint()
+                
+                confirmar_presença_button.click()
         else:
             print("comparação de token valida")
+            
 
 
     system('clear')
@@ -65,7 +67,7 @@ def main():
     print('passou pelo input')
     acessandoSite()
     print('passou pela acessando site')
-    login(credenciais["cpf"], credenciais["aniver"])
+    login(credenciais["user_cpf"], credenciais["user_date"])
     print('passou pelo login')
     acessandoPresenca()
     print('passou pela presença')
